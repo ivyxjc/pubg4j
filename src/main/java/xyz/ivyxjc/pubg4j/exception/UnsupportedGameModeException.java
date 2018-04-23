015f6539478b5e5a649b09f524b6f3d97c69f0c3
ivyxjc <ivyxjc1994@hotmail.com>
2018-04-24 00:28:29 +0800

    :sparkles: add neede exception

---
+++ src/main/java/xyz/ivyxjc/pubg4j/exception/UnsupportedGameModeException.java
@@ -0,0 +1,11 @@
+package xyz.ivyxjc.pubg4j.exception;
+
+/**
+ * @author Ivyxjc
+ * @since 4/23/2018
+ */
+public class UnsupportedGameModeException extends UnsupportedPubgElementException {
+    public UnsupportedGameModeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/xyz/ivyxjc/pubg4j/exception/UnsupportedMapException.java
@@ -0,0 +1,11 @@
+package xyz.ivyxjc.pubg4j.exception;
+
+/**
+ * @author Ivyxjc
+ * @since 4/23/2018
+ */
+public class UnsupportedMapException extends UnsupportedPubgElementException {
+    public UnsupportedMapException(String message) {
+        super(message);
+    }
+}

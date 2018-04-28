b00a5390348a4c044697a42277ccf17e0e588fbd
ivyxjc <ivyxjc1994@hotmail.com>
2018-04-28 20:06:42 +0800

    :sparkles: add druid monitor

--- src/main/java/xyz/ivyxjc/pubg4j/Pubg4jApplication.java
+++ src/main/java/xyz/ivyxjc/pubg4j/Pubg4jApplication.java
@@ -1,15 +1,21 @@
 package xyz.ivyxjc.pubg4j;
 
+import com.alibaba.druid.support.http.StatViewServlet;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import xyz.ivyxjc.pubg4j.httpclient.GetApi;
 import xyz.ivyxjc.pubg4j.service.PubgPlayerService;
@@ -19,6 +25,8 @@ import xyz.ivyxjc.pubg4j.service.PubgPlayerService;
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("xyz.ivyxjc.pubg4j.dao")
+@PropertySource(value = {"classpath:private.yaml", "classpath:application.yaml"},
+                ignoreResourceNotFound = false)
 public class Pubg4jApplication {
 
     @Autowired
@@ -26,6 +34,13 @@ public class Pubg4jApplication {
 
     @Autowired
     private PubgPlayerService mPubgPlayerService;
+
+    @Value("${druid_user}")
+    private String druidUsername;
+
+    @Value("${druid_password}")
+    private String druidPassword;
+
 
     public static void main(String[] args) {
         SpringApplication.run(Pubg4jApplication.class, args);
@@ -53,4 +68,20 @@ public class Pubg4jApplication {
     public CloseableHttpClient httpClient(RequestConfig requestConfig) {
         return HttpClients.custom().setDefaultRequestConfig(requestConfig).build();
     }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("resetEnable", "false"); //禁用HTML页面上的“Rest All”功能
+        initParameters.put("allow", "");  //ip白名单（没有配置或者为空，则允许所有访问）
+        initParameters.put("deny", ""); //ip黑名单
+        initParameters.put("loginUsername", druidUsername);  //++监控页面登录用户名
+        initParameters.put("loginPassword", druidPassword);  //++监控页面登录用户密码
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
+    }
+
 }
